852d3cfa60f98cf1f7f88fb96d23b7d1b0119924
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2023-07-05 13:09:23 +0330

    feat(popularmovie): add getpopularseries func to get list of series

--- src/components/Series.vue
+++ src/components/Series.vue
@@ -1,70 +1,38 @@
 <template>
-    <section id="movies" class="movies">
-      <h2 class="heading">Series</h2>
-  
-      <div class="movie-container">
-        <div v-for="(series, index) in seriesList" :key="index" class="box">
-          <div class="box-img">
-            <img :src="series.image" :alt="series.title">
-          </div>
-          <h3>{{ series.title }}</h3>
-          <span>{{ series.info }}</span>
+  <section id="movies" class="movies">
+    <h2 class="heading">Series</h2>
+
+    <div class="movie-container">
+      <div v-for="(series, index) in seriesList" :key="index" class="box">
+        <div class="box-img">
+          <img :src="series.poster_path" :alt="series.title">
         </div>
+        <h3>{{ series.title }}</h3>
+        <span>{{ series.info }}</span>
       </div>
-    </section>
-  </template>
+    </div>
+  </section>
+</template>
   
-  <script setup>
-    import breakingbad from '../assets/images/breakingbad.jpg';
-    import friends from '../assets/images/Friends.jpg';
-    import moneyheist from '../assets/images/moneyheist.jpg';
-    import walkingdead from '../assets/images/walkingdead.jpg';
-    import got from '../assets/images/GameofThrons.jpg';
-    import rickandmorty from '../assets/images/rickandmorty.jpg';
-    import motaham from '../assets/images/motaham.jpg';
-    import chernobyl from '../assets/images/chernobyl-ka-1920.jpg';
+<script setup>
 
+import { onMounted, ref } from 'vue';
+const seriesList = ref([]);
 
-  const seriesList = [
-    {
-      title: 'Breaking Bad',
-      image: breakingbad,
-      info: '5 Season | Drama'
-    },
-    {
-      title: 'Friends',
-      image: friends,
-      info: '10 Season | Fun'
-    },
-    {
-      title: 'Money Heist',
-      image: moneyheist,
-      info: '5 Season | Adventure'
-    },
-    {
-      title: 'Walking Dead',
-      image: walkingdead,
-      info: '11 Season | Action'
-    },
-    {
-      title: 'Game of Thrones',
-      image: got,
-      info: '8 Series | Action'
-    },
-    {
-      title: 'Rick and Morty',
-      image: rickandmorty,
-      info: '6 Season | Drama'
-    },
-    {
-      title: 'Motaham Gorikht',
-      image:motaham,
-      info: '20 episodes | Comedy (suggestion)'
-    },
-    {
-      title: 'Chernobyl',
-      image: chernobyl,
-      info: '1 Season | Action'
-    }
-  ];
-  </script>
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTU2YzhmYzk2N2EzMTA1NDk2OTA3MmFhY2E2MDVmNiIsInN1YiI6IjY0YTUxYjMyMWJmMjY2MDEwNTE4MDAyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HxmHxMHAYsrdt_Q3S613yYxLtOJCHd2gYTqAfOAIY2I'
+  }
+};
+
+const getPopularSeries = async () => {
+  const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options);
+  const data = await response.json();
+  seriesList.value = data.results.slice(0, 8) ;
+};
+
+onMounted(getPopularSeries);
+
+</script>
