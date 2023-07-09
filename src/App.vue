45bf2dec2b12eb942ad09acd2fb012db455fac01
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2023-07-09 23:36:31 +0330

    refactor(MovieApp): remove unneccessory code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
-
-
-
-<!-- <template>
-  <h1>{{ message }}</h1>
-</template>
-
-<script setup >
-  const message = "AliBaba Academy"
-</script> -->
-
 <template>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <div id="app">
-    <!-- <Header />
+    <Header />
     <Home />
     <Search />
     <Series />
     <Movies />
-    <Footer /> -->
-    <!-- <Login /> -->
-    <Register />
+    <Footer />
   </div>
 </template>
 
@@ -31,6 +18,4 @@
   import Series from './components/Series.vue';
   import Movies from './components/Movies.vue';
   import Footer from './components/Footer.vue';
-import Register from './components/Register.vue';
-  // import Login from './components/login.vue'
 </script>
