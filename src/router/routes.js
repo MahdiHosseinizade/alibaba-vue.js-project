bb6b77d6070362f8de3714f81447f12d846006e6
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2023-07-22 17:07:12 +0330

    feat(watchlist): add route of watchlist

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,11 +5,12 @@ const Register = defineAsyncComponent(() => import('@/components/Register.vue'))
 const MovieDetail = defineAsyncComponent(() => import('@/components/MovieDetail.vue'));
 const Login = defineAsyncComponent(() => import('@/components/login.vue'));
 const Profile = defineAsyncComponent(() => import('@/pages/profile.vue'));
-
+const WatchList =defineAsyncComponent(() =>import('@/pages/watchlist.vue'));
 export const routes = [
   { path: '/', name: 'home', component: Home  },
   { path: '/login', name: 'login', component: Login },
   { path: '/register', name: 'register', component: Register },
   { path: '/movie/:id', name: 'movie', component: MovieDetail },
+  {path : '/watchlist',name:'watchlist',component:WatchList},
   { path: '/profile', name: 'profile', component: Profile , meta: { requiresAuth: true }}
 ];
