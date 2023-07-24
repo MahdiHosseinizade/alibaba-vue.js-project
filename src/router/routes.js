ef9842f4fdcd669a4764789a1c617cc46d69b1ff
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2023-07-25 01:05:19 +0330

    refactor(Movie): move register and login to pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,10 @@
 import { defineAsyncComponent } from 'vue';
 
 const Home = defineAsyncComponent(() => import('@/components/Home.vue'));
-const Register = defineAsyncComponent(() => import('@/components/Register.vue'));
+const Register = defineAsyncComponent(() => import('@/pages/Register.vue'));
 const MovieDetail = defineAsyncComponent(() => import('@/components/MovieDetail.vue'));
-const Login = defineAsyncComponent(() => import('@/components/login.vue'));
-const Profile = defineAsyncComponent(() => import('@/pages/Profile.vue'));
+const Login = defineAsyncComponent(() => import('@/pages/login.vue'));
+const Profile = defineAsyncComponent(() => import('../pages/Profile.vue'));
 const WatchList =defineAsyncComponent(() =>import('@/pages/Watchlist.vue'));
 export const routes = [
   { path: '/', name: 'home', component: Home  },
@@ -12,5 +12,5 @@ export const routes = [
   { path: '/register', name: 'register', component: Register },
   { path: '/movie/:id', name: 'movie', component: MovieDetail },
   {path : '/watchlist',name:'watchlist',component:WatchList},
-  { path: '/profile', name: 'profile', component: Profile , meta: { requiresAuth: true }}
+  { path: '/profile', name: 'profile', component: Profile }
 ];
