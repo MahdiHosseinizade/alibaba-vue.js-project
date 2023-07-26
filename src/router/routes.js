ec0d912a47eefd4c6d420b8e8c2bc67bb4d8a166
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2023-07-26 14:28:35 +0330

    feat(deploy)

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,14 +5,14 @@ const Register = defineAsyncComponent(() => import('@/pages/Register.vue'));
 const MovieDetail = defineAsyncComponent(() => import('@/pages/MovieDetail.vue'));
 const Login = defineAsyncComponent(() => import('@/pages/login.vue'));
 const profile = defineAsyncComponent(() => import('@/pages/profile.vue'));
-const WatchList =defineAsyncComponent(() =>import('@/pages/watchlist.vue'));
-const Search = defineAsyncComponent(() => import('@/pages/searchMovie.vue'));
+const watchList =defineAsyncComponent(() =>import('@/pages/watchlist.vue'));
+const search = defineAsyncComponent(() => import('@/pages/searchMovie.vue'));
 export const routes = [
   { path: '/', name: 'home', component: Home  },
   { path: '/login', name: 'login', component: Login },
   { path: '/register', name: 'register', component: Register },
   { path: '/movie/:id', name: 'movie', component: MovieDetail },
-  {path: '/search', name: 'search', component: Search},
-  {path : '/watchlist',name:'watchlist',component:WatchList,meta: { requiresAuth: true }},
+  {path: '/search', name: 'search', component: search},
+  {path : '/watchlist',name:'watchlist',component:watchList,meta: { requiresAuth: true }},
   { path: '/profile', name: 'profile', component: profile , meta: { requiresAuth: true }},
 ];
