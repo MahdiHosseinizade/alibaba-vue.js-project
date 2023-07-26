2a42fcd0695fd5de8c8721f96b1b24177cf01ab6
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2023-07-26 13:23:34 +0330

    feat(profile): add spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,7 @@
+<template>
+    <pulse-loader  :loading="loading" color="yellow" :size="size"></pulse-loader>
+</template>
+  
+<script setup>
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+</script>
